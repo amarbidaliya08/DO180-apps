5e5c77dbfbd763e77cd09622a008aa2ca5c0a8fb
Student User <student@workstation.lab.example.com>
2022-08-23 04:50:12 -0400

    New changes in php content

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Welecome to D0180 Course . "\n";
+#print "Welecome to D0180 Course . "\n";
+print "A change is a coming!\n";
 ?>
